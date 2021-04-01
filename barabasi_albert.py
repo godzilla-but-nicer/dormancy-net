ac7da05389f918caa4232a6d119fe443c032e2d7
godzilla-but-nicer <patgwall@iu.edu>
2021-04-01 11:07:42 -0400

    BA dormancy unfinished

--- barabasi_albert.py
+++ barabasi_albert.py
@@ -35,7 +35,7 @@ def barabasi_albert(N, m):
         degrees = np.array([d[1] for d in G.degree()])
         pa_probs = degrees / np.sum(degrees)
         # make connections
-        targets = rng.choice(range(degrees.shape[0]), p=pa_probs, size=m)
+        targets = rng.choice(range(degrees.shape[0]), p=pa_probs, size=m, replace=False)
         new_edges = [(i, t) for t in targets]
         G.add_edges_from(new_edges)
 
@@ -50,7 +50,7 @@ nx.draw_spring(G)
 # distributions but we'll just go with the classic PDF like they do in the paper
 # %%
 m = 3
-G = barabasi_albert(10000, m)
+G = barabasi_albert(1000, m)
 degrees = [d[1] for d in G.degree()]
 sorted_degree = sorted(degrees)
 
@@ -75,4 +75,56 @@ plt.ylabel('P(k)')
 plt.xscale('log')
 plt.yscale('log')
 plt.show()
+# %% [markdown]
+# Looks good! Now that our BA model is working we can think about the role of
+# dormancy. We'll start by implementing the model from Wang *et al.*
+# we're going to add nodes one at a time like in BA model. These nodes will
+# attach with preferrential attachment. but we're going to put to sleep and 
+# wake up nodes at each time step. 
+# 
+# The process looks like this:
+# 
+# 1. Add new node with m connections [maybe with preferrential attachment?] only to active nodes
+# 1. Set incoming node state to active
+# 1. Randomly wake up a node
+# 1. Set two nodes to the dormant state with probability:
+#  $$ \nu_j = \frac{\gamma - 1}{\alpha + k^{in}_j} $$
+#   where,
+#  $$ \gamma - 1 = \left [ \sum_{l \in A} \frac{1}{\alpha + k^{in}_l} \right ]^{-1} $$
+#
+# so yeah let's try and implement this thing
+# %%
+def BA_dormancy(N, m, alpha):
+    qfn = 2
+    G = nx.DiGraph()
+    
+    # start with the initial m nodes and add the next node
+    G.add_nodes_from(range(m), dormant=False)
+    init_edges = [(i, m+1) for i in range(m)]
+    G.add_edges_from(init_edges)
+    # need dormancy vector for downstream operations
+    ddict = nx.get_node_attributes(G, 'dormant')
+    dormancy = np.array([ddict[i] for i in len(ddict)])
+
+    # now add the rest
+    for i in range(m + 2, N):
+        # connect randomly if node is not dormant, 
+        targets = rng.choice(where(dormant == False), size=m, replace=False)
+        new_edges = [(i, t) for t in targets]
+    
+        # wake up one dormant node
+        wake_up = rng.choice(where(dormant), size=qfn - 1, replace=False)
+
+        # make dormant two nodes according to probability above
+
+
+        indeg = np.array([d[1] for d in G.in_degree()])
+        pa_probs = indeg / np.sum(indeg)
+        G.add_edges_from(new_edges)
+
+    return G
+G = BA_dormancy(50, 5, 10)
+nx.draw_spring(G)
+# %%
+
 # %%
